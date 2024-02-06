a340dbef8bbb78250a6e374162af06b9008bf041
marco-soria <marco.soria@pucp.edu.pe>
2024-02-06 11:29:33 -0500

    events and 1usestate

--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -1,12 +1,23 @@
+import React from 'react'
 import './TodoSearch.css'
 
 function TodoSearch() {
-    return (
-      <input 
-      placeholder="Cut onions"
-      className='TodoSearch'
-      />
-    )
-  }
+  const [searchValue, setSearchValue] = React.useState('')
+
+  console.log(`THe users wrote ${searchValue}`)
+
+  return (
+    <input 
+    placeholder="Cut onions"
+    className='TodoSearch'
+    value={searchValue}
+    onChange= {
+      (event) => {
+        setSearchValue(event.target.value)
+      }	
+    }
+    />
+  )
+}
   
   export { TodoSearch }
